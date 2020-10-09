38cb08810e6c0149a94f5b5a41ecf4847633e7eb
Forrest Reiland <you@example.com>
2020-10-09 09:27:40 -0700

    fix typo for all spec test, TEST PASS

--- lib/project.rb
+++ lib/project.rb
@@ -30,7 +30,7 @@ class Project
     returned_projects.each() do |project|
       title = project.fetch("title")
       id = project.fetch("id").to_i
-      projects.push(Project.new({:name => name, :id => id}))
+      projects.push(Project.new({:title => title, :id => id}))
     end
     projects
   end 
